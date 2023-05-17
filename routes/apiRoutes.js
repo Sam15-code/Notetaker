4349a16f17bdeeb4469eebf205aadeb5aec2575f
Sam15-code <ibtissam.dxb@gmail.com>
2023-05-16 21:23:33 -0500

    fixed errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,30 +3,28 @@ const fs = require("fs");
 let db = require("../db/db.json")
 
 
-router.get("/notes",function(req,res){
-     fs.readFile('db/db.json', (err, data) => {
-        if(err) throw err;
-        res.json(JSON.parse(data));
-     })
+router.get("/notes", function (req, res) {
+    fs.readFile('db/db.json', (err, data) => {
+        if (err) throw err;
+        db =JSON.parse(data)
+        res.json(db)
+    })
 });
 //req.body
 //req.param
-router.post("/notes",function(req,res){
+router.post("/notes", function (req, res) {
 
     let newData = {
         title: req.body.title,
         text: req.body.text,
-        id: Math.floor(Math.random()*1000289)
+        id: Math.floor(Math.random() * 1000289)
     }
-
-    fs.writeFile('db/db.json', (err, data) => {
-        if(err) throw err;
-        const parseData= JSON.parse(data);
-        parseData.push(newData)
-        fs.writeFile("db/db.json",JSON.stringify(parseData),
-     err=>err ? console.log(err):res.redirect("/notes"))
-
-})
+    db.push(newData);
+    fs.writeFile('db/db.json',JSON.stringify(db), (err, data) => {
+        if (err) throw err;
+        
+    })
+    res.json(db)
 })
 module.exports = router;
 
